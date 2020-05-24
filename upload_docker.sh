608bd95c7146da0dd14d902024fd6a2b193f4243
EC2 Default User <ec2-user@ip-172-31-60-23.ec2.internal>
2020-05-24 21:23:40 +0000

    update the project

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=jaganmohangumpa/microservices
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+docker login --username jaganmohangumpa
+docker tag microservices $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
