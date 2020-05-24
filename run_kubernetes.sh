9b798d4eb70ef9094c462e957eb3494574a9894c
Jagan Mohan <65647352+aws-cloud9@users.noreply.github.com>
2020-05-25 03:02:18 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,7 +5,7 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=jaganmohangumpa/microservices
+dockerpath='jaganmohangumpa/microservices:latest'
 
 # Step 2
 # Run the Docker Hub container with kubernetes
